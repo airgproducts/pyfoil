e6122f7d4f5062ed3d5e2a60e0db7ec4021c1406
hiaselhans <simon.klemenc@gmail.com>
2021-07-26 08:47:29 +0200

    bump version

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ class CMakeBuild(build_ext):
 
         shutil.copytree("pyfoil", stubgen_path+"/pyfoil")
 
-version = "0.1.0"
+version = "0.1.1"
 
 with open("README.md") as readme_file:
     long_description = readme_file.read()

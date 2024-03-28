9616be125cd2fe8b281bbb0c29a69b96a290a814
hiaselhans <simon.klemenc@gmail.com>
2024-03-28 16:56:38 +0100

    bump version

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class CMakeBuild(build_ext):
 
         subprocess.check_call([sys.executable, 'stubs.py', stubgen_path])
 
-version = "0.1.6"
+version = "0.1.7"
 
 with open("README.md") as readme_file:
     long_description = readme_file.read()
--- src/version.hpp
+++ src/version.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-const char* version = "0.1.5";
+const char* version = "0.1.7";
